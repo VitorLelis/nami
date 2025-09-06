27ec4ce28a0b438665018413085bc329ccb272d0
VitorLelis <vitorll909@gmail.com>
2025-09-06 22:19:08 +0100

    feat(tags): delete tags

--- app/tags.tsx
+++ app/tags.tsx
@@ -4,20 +4,33 @@ import { useEffect, useState } from 'react';
 import { Tag, useDatabase } from '@/db/useDatabase';
 import { useFocusEffect } from 'expo-router';
 import React from 'react';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 import TagCard from '@/components/TagCard';
 import AddButton from '@/components/AddButton';
 import AddTagModal from '@/components/AddTagModal';
+import MessageModal from '@/components/MessageModal';
 
 export default function TagScreen() {
-    const [tagList,setTagList] = useState<Tag[]>([])
-    const [addVisible,setAddVisible] = useState(false)
+  const [tagList,setTagList] = useState<Tag[]>([])
+  const [addVisible,setAddVisible] = useState(false)
+  const [deleteVisible,setDeleteVisible] = useState(false)
+  const [selectedId, setSelectedId] = useState(0)
 
   const db = useDatabase();
 
   async function getTags() {
     const tags = await db.getTagList()
     setTagList(tags)
+  }
+
+  async function handleDelete() {
+    try {
+      await db.deleteTag(Number(selectedId));
+    } catch (error) {
+      Alert.alert('Error', String(error));
+    }
+    setDeleteVisible(false)
+    getTags();
   }
 
   useEffect(() => {
@@ -36,7 +49,16 @@ export default function TagScreen() {
       <AddTagModal visible={addVisible} onClose={() => {
         setAddVisible(false);
         getTags();
-        }}/>
+        }}
+      />
+
+      <MessageModal 
+        visible={deleteVisible}
+        message='DELETE THE TAG?'
+        buttonText='DELETE'
+        onPress={handleDelete}
+        onClose={() => setDeleteVisible(false)}
+      />
 
       <ScreenTitle title= "Tags" subtitle='Manage all your tags'/>
 
@@ -48,7 +70,10 @@ export default function TagScreen() {
             renderItem={({ item }) => (
               <TagCard tag={item} 
                 onEdit={() => console.log("edit tag")} 
-                onDelete={() => console.log("delete tag")} 
+                onDelete={() => {
+                  setSelectedId(item.id);
+                  setDeleteVisible(true)
+                }} 
               />
               
             )}
